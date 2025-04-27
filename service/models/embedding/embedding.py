a793e7bdedf436e1a98c860e7a2a6d0f6432c6cd
Sh1ntaro <epev.nd@mail.ru>
2025-04-27 14:56:40 +0300

    + add annotation parameters

--- service/models/embedding/embedding.py
+++ service/models/embedding/embedding.py
@@ -1,25 +1,25 @@
 # --- embeddings.py ---
-
+from typing List, Optional
 import torch
 from transformers import AutoTokenizer, AutoModel
 from langchain.embeddings.base import Embeddings as BaseEmbeddings
 from tqdm import tqdm
 
-DEFAULT_MODEL_NAME = "intfloat/multilingual-e5-large"
+DEFAULT_MODEL_NAME str = "intfloat/multilingual-e5-large"
 
 class EmbeddingModel(BaseEmbeddings):
-    def __init__(self, model_name: str = DEFAULT_MODEL_NAME, device: str = None):
-        self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
+    def __init__(self, model_name: str = DEFAULT_MODEL_NAME, device: Optional[str] = None) -> None:
+        self.device str = device or ("cuda" if torch.cuda.is_available() else "cpu")
         try:
             self.tokenizer = AutoTokenizer.from_pretrained(model_name)
             self.model = AutoModel.from_pretrained(model_name).to(self.device)
         except Exception as e:
             raise RuntimeError(f"Ошибка загрузки модели эмбеддингов '{model_name}': {e}")
 
-    def embed_documents(self, texts, batch_size=10):
-        all_embeddings = []
+    def embed_documents(self, texts: List[str], batch_size: int = 10) -> List[List[float]]:
+        all_embeddings = List[List[float]] = []
         for i in tqdm(range(0, len(texts), batch_size), desc="Эмбеддинг батчей", unit="батч"):
-            batch = texts[i:i + batch_size]
+            batch: List[str] = texts[i:i + batch_size]
             inputs = self.tokenizer(batch, padding=True, truncation=True, return_tensors="pt").to(self.device)
             with torch.no_grad():
                 model_output = self.model(**inputs)
@@ -27,7 +27,7 @@ class EmbeddingModel(BaseEmbeddings):
             all_embeddings.extend(embeddings.cpu().numpy())
         return all_embeddings
 
-    def embed_query(self, text):
+    def embed_query(self, text: str) -> List[float]:
         return self.embed_documents([text])[0]
 
     @staticmethod
