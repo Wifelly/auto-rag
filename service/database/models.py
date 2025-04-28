b87d64d233b287b767b104ff02bb0f6fd5083a20
Sh1ntaro <epev.nd@mail.ru>
2025-04-28 21:26:48 +0300

    + add in class Embedding: vector_db_path

--- service/database/models.py
+++ service/database/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from typing import Optional
-from sqlalchemy import ForeignKey
+
+from sqlalchemy import ARRAY, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
+
 from .config import Base
 
 
@@ -23,19 +24,14 @@ class Embedding(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(nullable=False)
-    file: Mapped[str] = mapped_column(nullable=False)
+    files: Mapped[list[str]] = mapped_column(ARRAY(String), nullable=False)
     status_id: Mapped[int] = mapped_column(ForeignKey("embedding_status.id"))
-    created_at: Mapped[datetime] = mapped_column(
-        server_default=func.now(),
-        nullable=False
-    )
-    updated_at: Mapped[Optional[datetime]] = mapped_column(
-        onupdate=func.now(),
-        nullable=True
-    )
+    vector_db_path: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime | None] = mapped_column(onupdate=func.now(), nullable=True)
 
     # Relationship to EmbeddingStatus
     status_rel: Mapped[EmbeddingStatus] = relationship(back_populates="embeddings")
 
     def __repr__(self) -> str:
-        return f"Embedding(id={self.id}, name={self.name}, file={self.file})"
+        return f"Embedding(id={self.id}, name={self.name}, files={self.files}), vector_db_path={self.vector_db_path})"
