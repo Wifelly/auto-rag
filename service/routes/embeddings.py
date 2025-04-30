1d979b1446c720634997e1cc5c5d76350a30a1d5
Sh1ntaro <epev.nd@mail.ru>
2025-04-30 08:18:45 +0300

    link to task

--- service/routes/embeddings.py
+++ service/routes/embeddings.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException, UploadFile
-from pydantic import BaseModel
+from fastapi import APIRouter, BackgroundTasks, Depends, File, Form, HTTPException, UploadFile
+from pydantic import BaseModel, ConfigDict
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from service.database.config import get_db
+from service.service.tasks import train_task
 from service.services.embedding_service import EmbeddingService
 
 router = APIRouter()
@@ -20,14 +21,22 @@ class EmbeddingResponse(BaseModel):
     vector_db_path: str
     status_id: int
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
 
 
 @router.post("/embeddings/", response_model=EmbeddingResponse)
-async def create_embedding(embedding: EmbeddingCreate, files: list[UploadFile], db: AsyncSession = Depends(get_db)):
+async def create_embedding(
+    name: str = Form(...),
+    vector_db_path: str = Form(...),
+    status_id: int = Form(...),
+    files: list[UploadFile] = File(...),
+    db: AsyncSession = Depends(get_db),
+):
+    file_names = [file.filename for file in files]
     embedding_service = EmbeddingService(db)
-    return await embedding_service.create_embedding(name=embedding.name, files=files, status_id=embedding.status_id)
+    return await embedding_service.create_embedding(
+        name=name, files=file_names, status_id=status_id, vector_db_path=vector_db_path
+    )
 
 
 @router.get("/embeddings/{embedding_id}", response_model=EmbeddingResponse)
@@ -61,3 +70,30 @@ async def update_embedding_status(embedding_id: int, status_id: int, db: AsyncSe
     if embedding is None:
         raise HTTPException(status_code=404, detail="Embedding not found")
     return embedding
+
+
+@router.post("/embeddings/train", tags=["Embedding"])
+async def train_embedding(
+    background_tasks: BackgroundTasks,
+    user_docs_dir: str,
+    output_dir: str,
+    chunk_size: int = 1024,
+    chunk_overlap: int = 200,
+    max_files: int | None = None,
+    append: bool = False,
+    db: AsyncSession = Depends(get_db),
+):
+    try:
+        background_tasks.add_task(
+            train_task,
+            user_docs_dir=user_docs_dir,
+            output_dir=output_dir,
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            max_files=max_files,
+            append=append,
+            db=db,
+        )
+        return {"message": "Процесс обучения запущен в фоне."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Ошибка при запуске обучения: {e}")
