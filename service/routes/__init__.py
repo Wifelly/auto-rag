6dfb7d350dfb546297ac4b0b13f629e6b881c555
Sh1ntaro <epev.nd@mail.ru>
2025-05-08 09:24:09 +0300

    remove ws, add chat

--- service/routes/__init__.py
+++ service/routes/__init__.py
@@ -1,4 +1,5 @@
+from service.routes.chat import router as chat_router
 from service.routes.health import router as health_router
 from service.routes.metrics import router as metrics_router
 
-__all__ = ["health_router", "metrics_router"]
+__all__ = ["health_router", "metrics_router", "chat_router"]
